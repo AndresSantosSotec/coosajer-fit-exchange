6318706c94f560a0d7170d630a29abbe76f3b38f
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-09-02 16:34:20 -0600

    asdf

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/store/', // Esta línea es crucial
   server: {
     host: "::",
     port: 8080,
